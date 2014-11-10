3ae8c8dfccffecc4d9771f944a7430b4ffd672ed
PriyaMurali <PriyaMurali@users.noreply.github.com>
2014-11-10 12:48:02 +0530

    Update plot1.R

--- Course Project 2/plot1.R
+++ Course Project 2/plot1.R
@@ -8,7 +8,7 @@ NEIsample <- NEI[sample(nrow(NEI), size = 1000, replace = F), ]
 # Aggregates
 Emissions <- aggregate(NEI[, 'Emissions'], by = list(NEI$year), FUN = sum)
 Emissions$PM <- round(Emissions[, 2] / 1000, 2)
-
+#Png file
 png(filename = "plot1.png")
 barplot(Emissions$PM, names.arg = Emissions$Group.1, main = expression('Total Emission of PM'[2.5]), xlab = 'Year', ylab = expression(paste('PM', ''[2.5], ' in Kilotons')))
 dev.off()
