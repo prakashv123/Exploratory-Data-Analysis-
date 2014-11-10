3126fd4d32b2642795510067ef83d27f786550df
PriyaMurali <PriyaMurali@users.noreply.github.com>
2014-11-10 12:49:13 +0530

    Update plot2.R

--- Course Project 2/plot2.R
+++ Course Project 2/plot2.R
@@ -10,4 +10,4 @@ MD <- subset(NEI, fips == '24510')
 
 png(filename = 'plot2.png')
 barplot(tapply(X = MD$Emissions, INDEX = MD$year, FUN = sum), main = 'Total Emission in Baltimore City, MD', xlab = 'Year', ylab = expression('PM'[2.5]))
-dev.off()
+dev.off() 
